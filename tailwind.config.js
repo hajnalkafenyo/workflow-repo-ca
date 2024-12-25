4a124588032b2585210c71c1520c06d424cd643b
hajnalkafenyo <77206669+hajnalkafenyo@users.noreply.github.com>
2024-12-25 18:28:46 +0100

    Add ESLint configuration and update dependencies; refactor Tailwind CSS config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./**/*.html", "./js/**/*.js", "!./node_modules/**/*"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+export const content = ["./**/*.html", "./js/**/*.js", "!./node_modules/**/*"];
+export const theme = {
+  extend: {},
 };
+export const plugins = [];
